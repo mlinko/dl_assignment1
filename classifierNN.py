74614ac8432ef73dd2a27056eee8b238e52ad317
Peter Mlinko <m.peter95@gmail.com>
2017-11-10 16:44:43 +0100

    minimal logging

--- classifierNN.py
+++ classifierNN.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import numpy as np
+from datetime import datetime
 
 class classifierNN:
 	def __init__(self, dictionary):
@@ -13,7 +14,7 @@ class classifierNN:
 		x = xi.reshape(xi.shape[0], 3072)
 		y = np.zeros(x.shape[0], dtype=int)
 		for i in range( x.shape[0]):
-			print(i)
+			if i%2 == 0: print('[%s]: %5d th element'%(datetime.now().strftime('%Y.%m.%d %H:%M:%S'),i))
 			distances = np.sum( np.abs(self.X - x[i,:]), axis=1 )
 			y[i] = self.Y[np.argmin(distances)]
 		return y
@@ -26,7 +27,7 @@ if __name__ == '__main__':
 	Xtr, Ytr, Xte, Yte, dictionary = loadInputs('cifar-10-batches-py')
 	nn = classifierNN(dictionary)
 	nn.train(Xtr,Ytr)
-	guess = nn.predict(Xte[0:10,:])
-
+	guess = nn.predict(Xte[0:5,:])
+	guess.tofile('guessfile')
 	accuracy = np.mean(Yte[0:guess.shape[0]] == guess )
 	print('accuracy: ', accuracy)
