f481eab41fbd37e4ba7afb3421d0a16a054f1df2
Mlinkó Péter <m.peter95@gmail.com>
2017-11-11 17:38:36 +0100

    classifierNN with tensorflow now

--- classifierNN.py
+++ classifierNN.py
@@ -3,24 +3,60 @@ import numpy as np
 from datetime import datetime
 
 class classifierNN:
+	# this class is a Nearest Neighbour classifier, that uses only numpy
+	# made only for fun
 	def __init__(self, dictionary):
 		pass
 	
 	def train(self, X, Y):
+		# here by 'training' I mean adding the training set to the class
 		self.X = X.reshape(X.shape[0], 3072)
 		self.Y = Y
 
 	def predict(self, xi):
+		# prediciton is simply findng the smallest distance between the 
+		# train set and the actual test element, and add the label of the
+		# nearest object to the actual test element
 		x = xi.reshape(xi.shape[0], 3072)
 		y = np.zeros(x.shape[0], dtype=int)
 		for i in range( x.shape[0]):
-			if i%2 == 0: print('[%s]: %5d th element'%(datetime.now().strftime('%Y.%m.%d %H:%M:%S'),i))
+			if i%250 == 0: print('[%s]: %5d th element'%(datetime.now().strftime('%Y.%m.%d %H:%M:%S'),i))
 			distances = np.sum( np.abs(self.X - x[i,:]), axis=1 )
 			y[i] = self.Y[np.argmin(distances)]
 		return y
 			
 
+import tensorflow as tf
 
+class classifierNN:
+	def __init__(self):
+		# graph inputs for the tensorflow
+		self.xtr = tf.placeholder('int', [None, 3072])
+		self.xte = tf.placeholder('int', [3072])
+
+		# On the http://cs231n.github.io/classification/ website
+		# the L1 distance is being used, which I am implementing here too
+		self.distance = tf.reduce_sum(tf.abs(tf.add(self.xtr, tf.negative(self.xte))))
+		# prediciton is based on the nearest object
+		self.predict = tf.arg_min(distance, 0)
+		# the next line initializes hte variables (assigns their )
+		self.init = tf.global_variables_initializer()
+		
+	def train(self, Xtr, Ytr):
+		# real training cannot really be implemented here, so
+		# I just pass the train set to the object
+		self.Xtr = Xtr
+		self.Ytr = Ytr
+
+	def predict(self, Xte ):
+		# prediction is the variable we will return with
+		prediction = numpy.zeros([len(Xte)])
+		with tf.Session() as session:
+			for i in range(len(Xte)):
+				neareastNeighbour = session.run(self.pred, feed_dict={self.xtr: self.Xtr, self.xte:self.Xte[i,:]})
+				prediction[i] = self.Ytr[nearestNeighbour]
+		return prediction
+				
 if __name__ == '__main__':
 	from cifar10reader import loadInputs
 
